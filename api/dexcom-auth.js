9b104469c962be3a2ace5e3c7d8542624b2ba2fb
Tyraldev <jb.iemfre@gmail.com>
2026-05-28 16:05:07 +0200

    Create dexcom-auth.js

---
+++ api/dexcom-auth.js
@@ -0,0 +1,14 @@
+// Initiates Dexcom OAuth flow - redirects browser to Dexcom login
+const CLIENT_ID = process.env.DEXCOM_CLIENT_ID;
+const REDIRECT_URI = process.env.DEXCOM_REDIRECT_URI || "https://diab2.vercel.app/api/callback";
+const DEXCOM_API = "https://api.eu.dexcom.com";
+
+export default function handler(req, res) {
+  const authUrl = DEXCOM_API + "/v2/oauth2/login" +
+    "?client_id=" + CLIENT_ID +
+    "&redirect_uri=" + encodeURIComponent(REDIRECT_URI) +
+    "&response_type=code" +
+    "&scope=offline_access";
+
+  res.redirect(302, authUrl);
+}
